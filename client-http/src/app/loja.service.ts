b8f558bd28969566c90912f978e1f1f7c4684f1a
Weskley dos Santos Rocha <weskley.s.r@hotmail.com>
2020-06-05 21:09:48 -0300

    basicamente
    Humidle

--- client-http/src/app/loja.service.ts
+++ client-http/src/app/loja.service.ts
@@ -1,24 +1,32 @@
 import { Injectable } from '@angular/core';
-
+import { MatSnackBar } from '@angular/material'
 @Injectable({
   providedIn: 'root'
 })
 export class LojaService {
   valor: number = 1000;
-  constructor() { }
+  constructor(private alerta: MatSnackBar) { }
 
 comprar(descontar: number){
   if(descontar > this.valor){
    
   }else{
     this.valor = this.valor - descontar;
+    this.alerta.open("Comprou BitCoin: R$"+ descontar, "Ok",{
+      duration:5000,
+      horizontalPosition: "center"
+    });
   }
   
 }
 vender(receber: any){
   
   this.valor = this.atualizar() + parseInt(receber); 
-
+  this.alerta.open("Vendeu BitCoin: R$"+ receber , "Ok",{
+    duration:5000,
+   
+    horizontalPosition: "center"
+  });
 }
 
 atualizar(){
